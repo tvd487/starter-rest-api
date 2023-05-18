bfe30fa2c83cd1a3dca705baacfcabae9fb9c42f
Dien Truong <dien.truong@axonactive.vn>
2023-05-18 15:12:53 +0700

    get host from node env

--- env.ts
+++ env.ts
@@ -15,6 +15,8 @@
 import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
+  // @ts-ignore
+  PORT: process.env.PORT || 3000,
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
 	CACHE_VIEWS: Env.schema.boolean(),
